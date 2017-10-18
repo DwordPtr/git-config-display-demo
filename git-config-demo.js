513fc8f1f9972aeb151d8a904320abf6125e5161
Bryan Tidwell <btidwell4@my.apsu.edu>
2017-10-18 12:07:35 -0500

    fix dep bug and take away ugly red json

--- git-config-demo.js
+++ git-config-demo.js
@@ -2,13 +2,11 @@
 'use strict';
 const colors = require('colors/safe');
 const gitConfig = require('git-config');
-const commander = require('commander');
 
 gitConfig(function(err, config) {
 		if(err) return done(err);
 		console.log(colors.grey("Git name"));
 		console.log(colors.green(config.user.name));
 		console.log(colors.green(config.user.email));
-		console.log(colors.red(JSON.stringify(config)));
 });
 
